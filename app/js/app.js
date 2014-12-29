1835b813c875a1cfadf089e2cc2bf06fd0a2b0fd
Nicolas AGIUS <nicolas.agius@lps-it.fr>
2014-12-29 19:27:20 +0100

    Added simple mobile UI

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,74 @@
 // Heatman main Javascript file
+
+$(document).ready(function(){
+	init_update();
+
+	$("form :input").change(function(){
+		// Get parameters from the form element 
+		var values   = $(this).attr('id').split('-'),
+		    channel  = values[0],
+			mode     = values[1];
+
+		// Set the real mode to make API call
+		if (mode == "persistent")
+			mode=get_form_mode(channel);
+
+		// Set persistent option
+		var data="persistent=" + $("#"+channel+"-persistent").is(":checked");
+
+		// Call API to update mode
+		$.post("/api/channel/" + channel + "/" + mode, data, function(response, status) {
+			update_status(channel);
+		});
+		// TODO add error hanlder
+	});
+}); 
+
+function get_form_mode(channel)
+{
+	for (var mode of window.channels[channel]["modes"])
+	{
+		if ($("#"+channel+"-"+mode).is(":checked"))
+		{
+			return mode;
+		}
+	}
+	return null;
+}
+
+function update_status(channel)
+{
+	$.get("/api/channel/" + channel, function (data, status) {
+		// Set checked the current mode
+		for (var mode of window.channels[channel]["modes"])
+		{
+			$("#"+channel+"-"+mode).prop("checked", data["mode"] == mode);
+			$("#"+channel+"-"+mode).checkboxradio("refresh");
+		}
+
+		// Set checked the auto mode
+		$("#"+channel+"-auto").prop("checked", !data["override"]);
+		$("#"+channel+"-auto").checkboxradio("refresh");
+
+		// Set checked the persistent mode
+		$("#"+channel+"-persistent").prop("checked", data["persistent"]);
+		$("#"+channel+"-persistent").flipswitch("refresh");
+
+	});
+}
+
+function init_update()
+{
+	$.get("/api/channels", function (data, status) {
+		// Global variable loaded once
+		window.channels=data;
+
+		// Updated current status
+		for (var channel in window.channels)
+		{
+			update_status(channel);
+		}
+	});
+}
+
+// vim: ts=4:sw=4:ai:noet
